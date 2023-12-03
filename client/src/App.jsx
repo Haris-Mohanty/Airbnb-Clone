09e6ddd46964e8781e5a38a9f5ce8cee41c9c43e
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-03 15:00:39 +0530

    client/src/components/Header.jsx- Navbar code processing....

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  
   return (
     <>
-      
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,49 @@
-
 const Header = () => {
   return (
     <>
-      <header>
-        <a href="" className="logo"></a>
+      <header className="p-4 flex justify-between">
+        <a href="" className="flex items-center gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-8 h-6 -rotate-90"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+            />
+          </svg>
+
+          <span className="font-bold text-xl">airbnb</span>
+        </a>
+        <div className="flex border border-gray-300 rounded-full py-2 px-4">
+          <div>Anywhere</div>
+          <div>Any week</div>
+          <div>Add guests</div>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+          </button>
+        </div>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
