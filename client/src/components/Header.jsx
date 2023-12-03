45dedadcb545adc3b18b4b69ae31f0114ede4e6e
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-03 15:21:11 +0530

    client/src/components/Header.jsx- Navbar created.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,14 +26,14 @@ const Header = () => {
           <div>Any week</div>
           <div className="border-l border-gray-300"></div>
           <div>Add guests</div>
-          <button>
+          <button className="bg-primary text-white p-1 rounded-full">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth="1.5"
               stroke="currentColor"
-              className="w-6 h-6"
+              className="w-4 h-3"
             >
               <path
                 strokeLinecap="round"
@@ -42,6 +42,36 @@ const Header = () => {
               />
             </svg>
           </button>
+        </div>
+        <div className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-5 h-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+          <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6 relative top-1"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
         </div>
       </header>
     </>
