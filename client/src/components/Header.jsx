1194678fabf3e8633756bee41066e12b0de13cdc
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-03 15:06:11 +0530

    client/tailwind.config.js- Add colors in config.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -20,9 +20,11 @@ const Header = () => {
 
           <span className="font-bold text-xl">airbnb</span>
         </a>
-        <div className="flex border border-gray-300 rounded-full py-2 px-4">
+        <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
           <div>Anywhere</div>
+          <div className="border-l border-gray-300"></div>
           <div>Any week</div>
+          <div className="border-l border-gray-300"></div>
           <div>Add guests</div>
           <button>
             <svg
