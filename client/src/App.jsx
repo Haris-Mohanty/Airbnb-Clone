555349e5177c181cb50ba73f5a911b411dfa1f6a
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-11 22:27:52 +0530

    client/src/components/Page/PageNotFound.jsx- Page not found code processing...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="*" />
+      </Routes>
     </>
   );
 }
