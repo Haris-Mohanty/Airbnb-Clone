9a56edc2ac86397565b7472cc3e432097eecfda8
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-12 22:02:00 +0530

    client/src/components/Header.jsx- Login link add successfully.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
@@ -43,7 +44,10 @@ const Header = () => {
             </svg>
           </button>
         </div>
-        <div className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4">
+        <Link
+          to={"/login"}
+          className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -72,7 +76,7 @@ const Header = () => {
               />
             </svg>
           </div>
-        </div>
+        </Link>
       </header>
     </>
   );
