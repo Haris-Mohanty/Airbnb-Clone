88f8e1cc7da8276f31f45b76ec4c4b88ed5680b6
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-13 21:42:23 +0530

    client/src/index.css- Form designed.

--- client/src/components/Page/LoginPage.jsx
+++ client/src/components/Page/LoginPage.jsx
@@ -2,11 +2,11 @@ const LoginPage = () => {
   return (
     <>
       <div className="mt-4 ">
-        <h1 className="text-4xl text-center">Login</h1>
+        <h1 className="text-4xl text-center mb-4">Login</h1>
         <form className="max-w-md mx-auto">
           <input type="email" placeholder="your@email.com" />
           <input type="password" placeholder="Password" />
-          <button>Login</button>
+          <button className="primary">Login</button>
         </form>
       </div>
     </>
