bd15fc315ba216eabbcf905126f4c0d8b33d4e41
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-14 20:12:23 +0530

    client/src/components/Page/RegisterPage.jsx- Register form creating code processing...

--- client/src/components/Page/RegisterPage.jsx
+++ client/src/components/Page/RegisterPage.jsx
@@ -1,7 +1,15 @@
 function RegisterPage() {
   return (
     <>
-      <h1>Register</h1>
+      <div className="mt-4 grow flex items-center justify-around">
+        <div className="mb-64">
+        <h1>Register</h1>
+        <form className="max-w-md mx-auto">
+            <input type="email" placeholder="your@email.com" />
+            <input type="password" placeholder="your@email.com" />
+        </form>
+        </div>
+      </div>
     </>
   );
 }
