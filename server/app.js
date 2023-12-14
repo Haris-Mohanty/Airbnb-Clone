151bd186920137a95b03ba15edc48813b5494105
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-14 21:01:09 +0530

    server/app.js- Node server in dev mode and running on port 8080 successfully.

--- server/app.js
+++ server/app.js
@@ -1,1 +1,28 @@
 import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import cors from "cors";
+import morgan from "morgan";
+
+//********* DOTENV CONFIGURATION *****/
+dotenv.config();
+
+//********* REST OBJECT ********/
+const app = express();
+
+//********* MIDDLEWARE **********/
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+//********* MIDDLEWARE ROUTES **********/
+
+
+//********* PORTS AND LISTEN **********/
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(
+    `Node server running in ${process.env.DEV_MODE} mode on Port ${port}.`
+      .bgBrightMagenta.white
+  );
+});
