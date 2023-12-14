c5caa216eea3c1d30b28e57a2afea520b9e8be18
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-14 20:09:33 +0530

    client/src/App.jsx- Register routes added.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import PageNotFound from "./components/Page/PageNotFound";
 import Home from "./components/Page/Home";
 import LoginPage from "./components/Page/LoginPage";
 import Layout from "./components/Layout";
+import RegisterPage from "./components/Page/RegisterPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Route>
 
         <Route path="*" element={<PageNotFound />} />
