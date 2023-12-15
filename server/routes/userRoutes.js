6daacb3aa5293285adec675a5c65efaecc09c72a
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-15 14:06:14 +0530

    server/routes/userRoutes.js- Register routes created.

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,1 +1,11 @@
 import express from "express";
+
+//Router Obj
+const router = express.Router();
+
+//***** Create routes */
+//Register
+router.post("/register");
+
+//Export
+export default router;
