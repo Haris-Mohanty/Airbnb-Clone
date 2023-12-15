e89e8dcb0e162857d65ed26caef8e6d29491c03b
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-15 14:22:07 +0530

    server/model.userModel.js- Create model for user registration code fixed.

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -1,1 +1,21 @@
 import mongoose from "mongoose";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is Required!"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is Required!"],
+    unique: true,
+    validate: validator.isEmail,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is Required!"],
+  },
+});
+
+export default mongoose.model("user", userSchema);
