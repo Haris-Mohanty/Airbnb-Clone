affaa23bcce50724942c2eb45180f54ff47ab107
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-15 14:15:58 +0530

    server/controller/userController.js- Validation checked successfully.

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,7 +1,25 @@
+//****************** USER REGISTRATION ******************/
 export const registerUser = async (req, res, next) => {
   try {
-    res.json("Register ");
+    const { name, email, password } = req.body;
+
+    //Validation
+    if (!name || !email || !password) {
+      return res.status(422).json({
+        message: "Please Provide All Fields!",
+      });
+    }
+    if (password.length < 6) {
+      return res.status(422).json({
+        message: "Password length should be greater than 6 character!",
+      });
+    }
+
+
   } catch (err) {
-    console.log(err);
+    return res.status(500).json({
+      message: "Error in register user API!",
+      error: err.message,
+    });
   }
 };
