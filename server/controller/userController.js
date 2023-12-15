91d3b2a45fc5ca741b88dc48d9270b25ec7d83bf
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-15 14:08:12 +0530

    server/controller/userController.js- Create controller functio for user registration....

---
+++ server/controller/userController.js
@@ -0,0 +1,7 @@
+export const registerUser = async (req, res, next) => {
+  try {
+    res.json("Register ");
+  } catch (err) {
+    console.log(err);
+  }
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
+import { registerUser } from "../controller/userController";
 
 //Router Obj
 const router = express.Router();
 
 //***** Create routes */
 //Register
-router.post("/register");
+router.post("/register", registerUser);
 
 //Export
 export default router;
