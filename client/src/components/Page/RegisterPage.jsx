37ec7692c1860c3359b52472ac5a7914bed3bff2
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-16 11:59:56 +0530

    client/src/components/Page/RegisterPage.jsx- Form submitting...

--- client/src/components/Page/RegisterPage.jsx
+++ client/src/components/Page/RegisterPage.jsx
@@ -5,15 +5,34 @@ function RegisterPage() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+  };
   return (
     <>
       <div className="mt-4 grow flex items-center justify-around">
         <div className="mb-64">
           <h1 className="text-4xl text-center mb-4">Register</h1>
-          <form className="max-w-md mx-auto">
-            <input type="text" placeholder="Your Name" />
-            <input type="email" placeholder="your@email.com" />
-            <input type="password" placeholder="Password" />
+          <form className="max-w-md mx-auto" onSubmit={handleRegister}>
+            <input
+              type="text"
+              placeholder="Your Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="email"
+              placeholder="your@email.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <button className="primary">Register</button>
             <div className="text-center py-2 text-gray-500">
               Already have an account?{" "}
