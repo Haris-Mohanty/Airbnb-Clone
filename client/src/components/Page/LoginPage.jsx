0a5d18132fcf37729361cb9eea9ea12694c5be4c
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-16 16:57:59 +0530

    client/src/components/Page/LoginPage.jsx- Login code fixed.

--- client/src/components/Page/LoginPage.jsx
+++ client/src/components/Page/LoginPage.jsx
@@ -1,16 +1,37 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { loginUser } from "../../api/api";
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await loginUser(email, password);
+      alert("Login Success!");
+    } catch (err) {
+      alert(err.response.data.message);
+    }
+  };
   return (
     <>
       <div className="mt-4 grow flex items-center justify-around">
         <div className="mb-64">
           <h1 className="text-4xl text-center mb-4">Login</h1>
-          <form className="max-w-md mx-auto">
-            <input type="email" placeholder="your@email.com" />
-            <input type="password" placeholder="Password" />
+          <form className="max-w-md mx-auto" onSubmit={handleLogin}>
+            <input
+              type="email"
+              placeholder="your@email.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <button className="primary">Login</button>
             <div className="text-center py-2 text-gray-500">
               Do not have an account yet?{" "}
