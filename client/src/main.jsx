ef01b30720d2e9e9bcc16bbb1d89e8ffe07c284d
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-16 12:23:56 +0530

    client/src/main.jsx- Vite base url added.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_APP_BASE_URL;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
