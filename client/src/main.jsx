bb714032a7e0636dc776318b0298492291f161cf
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-17 14:16:09 +0530

    client/src/main.jsx- add default credentials.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
 axios.defaults.baseURL = import.meta.env.VITE_APP_BASE_URL;
+axios.defaults.withCredentials=true
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
