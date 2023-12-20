c33aac18d5e7584daaa4b705c7d323d6e2bb880c
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-20 16:08:27 +0530

    server- Cookie-parser installed.

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -132,6 +132,16 @@ export const getUser = async (req, res, next) => {
 //************ GET LOGIN USER PROFILE DETAILS **************/
 export const loginUserProfile = async (req, res, next) => {
   try {
+    // const { token } = req.cookies;
+
+    // if (token) {
+    //   jwt.verify(token, process.env.JWT_SECRET, {}, (err, res) => {
+    //     if (err) throw err;
+    //     res.json(res)
+    //   });
+    // }
+
+    console.log("Cookies:", req.cookies);
   } catch (err) {
     return res.status(500).json({
       message: "Internal Server Error!",
