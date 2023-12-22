e04e8c7e7bd20ccc77c38da906e269915dfb8387
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-22 09:49:28 +0530

    client/src/components/Page/Account.jsx- Add icons.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/account/:subpage?" element={<Account />} />
+            <Route path="/account/:subpage/:action" element={<Account />} />
           </Route>
 
           <Route path="*" element={<PageNotFound />} />
