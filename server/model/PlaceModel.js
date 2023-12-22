0f237567f2aaf10ed0ecb622b3b339c02aa515f7
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-22 09:30:00 +0530

    server/model/PlaceModel.js- Schema for create a place is created successfully.

--- server/model/PlaceModel.js
+++ server/model/PlaceModel.js
@@ -1,5 +1,19 @@
 import mongoose from "mongoose";
 
-const placeSchema = new mongoose.Schema({});
+const placeSchema = new mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: user,
+  },
+  title: String,
+  address: String,
+  photos: [String],
+  description: String,
+  perks: [String],
+  extraInfo: String,
+  checkIn: Number,
+  checkOut: Number,
+  maxGuests: Number,
+});
 
 export default mongoose.model("place", placeSchema);
