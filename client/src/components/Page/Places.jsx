a1d77f2868a360967013090ec00d560960a04124
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-22 09:53:02 +0530

    client/src/components/Page/Place.jsx- Add new place code processing.....

--- client/src/components/Page/Places.jsx
+++ client/src/components/Page/Places.jsx
@@ -1,30 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const Places = () => {
+  const { action } = useParams();
   return (
     <>
-      <div className="text-center">
-        <Link
-          className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
-          to={"/account/places/new"}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
+      {action !== "new" && (
+        <div className="text-center">
+          <Link
+            className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
+            to={"/account/places/new"}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 4.5v15m7.5-7.5h-15"
-            />
-          </svg>
-          Add new Place
-        </Link>
-      </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+            Add new Place
+          </Link>
+        </div>
+      )}
     </>
   );
 };
