8b9ab2ac57a1f505028d0071c021fb82abee5aa5
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-25 23:28:23 +0530

    server/controller/imageController.js- Image upload by link code processing.......

--- server/controller/imageController.js
+++ server/controller/imageController.js
@@ -1,1 +1,10 @@
-export const imageUploadByLink = async (req, res, next) => {};
+export const imageUploadByLink = async (req, res, next) => {
+  try {
+    console.log("object")
+  } catch (err) {
+    return res.status(500).json({
+      message: "Internal Server Error!",
+      error: err.message,
+    });
+  }
+};
--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -1,12 +1,11 @@
+import express from "express";
 import { imageUploadByLink } from "../controller/imageController.js";
-
-const express = require("express");
 
 //Rest Obj
 const router = express.Router();
 
 //Upload image route
-router.post("upload-by-link", imageUploadByLink);
+router.post("/upload-by-link", imageUploadByLink);
 
 //Export
 export default router;
