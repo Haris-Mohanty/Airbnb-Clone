15496a7aa8b6be9f7b970fd16428460baaf69611
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-25 23:19:22 +0530

    server/routes/imageRoutes.js- Create a imageRoutes.js file for create image upload by link routes created.

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -1,10 +1,12 @@
+import { imageUploadByLink } from "../controller/imageController.js";
+
 const express = require("express");
 
 //Rest Obj
 const router = express.Router();
 
 //Upload image route
-router.post("upload-by-link");
+router.post("upload-by-link", imageUploadByLink);
 
 //Export
 export default router;
