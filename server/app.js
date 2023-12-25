085ac943367803827289a5c8172019e596a24b19
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-25 23:21:07 +0530

    server/app.js- Middleware routes added.

--- server/app.js
+++ server/app.js
@@ -3,9 +3,10 @@ import dotenv from "dotenv";
 import colors from "colors";
 import cors from "cors";
 import morgan from "morgan";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import connectDB from "./database/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import imageRoutes from "./routes/imageRoutes.js";
 
 //********* DOTENV CONFIGURATION *****/
 dotenv.config();
@@ -17,16 +18,19 @@ connectDB();
 const app = express();
 
 //********* MIDDLEWARE **********/
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
 //********* MIDDLEWARE ROUTES **********/
 app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/image", imageRoutes);
 
 //********* PORTS AND LISTEN **********/
 const port = process.env.PORT || 8080;
