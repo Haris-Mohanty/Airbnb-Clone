fcd8cb08a8d9a9b756209a31e1cfe2f63baf034c
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-26 23:31:39 +0530

    server/controller/imageController.js- Controller function code processing for image download and save in uploads file....

--- server/controller/imageController.js
+++ server/controller/imageController.js
@@ -1,6 +1,11 @@
+import download from "image-downloader";
 export const imageUploadByLink = async (req, res, next) => {
   try {
-    console.log("object")
+    const { link } = req.body;
+    download.image({
+      url: link,
+      dest: "uploads",
+    });
   } catch (err) {
     return res.status(500).json({
       message: "Internal Server Error!",
