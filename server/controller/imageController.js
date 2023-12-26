53dbd358cd304f04933b06085bffefb3ac1730c5
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-26 23:47:22 +0530

    server/controller/imageController.js- Path added.

--- server/controller/imageController.js
+++ server/controller/imageController.js
@@ -1,11 +1,23 @@
 import download from "image-downloader";
+
 export const imageUploadByLink = async (req, res, next) => {
   try {
     const { link } = req.body;
-    download.image({
+    const newName = Date.now() + ".jpg";
+    const dest = path.join(__dirname, "../uploads", newName);
+    console.log(dest)
+
+    return false;
+
+    const options = {
       url: link,
-      dest: "uploads",
-    });
+      dest: "../uploads" + newName,
+    };
+
+    await download
+      .image(options)
+      .then(({ fileName }) => console.log("Saved to", fileName))
+      .catch((err) => console.log(err));
   } catch (err) {
     return res.status(500).json({
       message: "Internal Server Error!",
