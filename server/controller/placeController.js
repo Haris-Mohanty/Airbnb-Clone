6c928dd6b44466f96bfd406674ddfe99330f1800
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-30 12:26:06 +0530

    server/controller/placeController.js- Create a place controller file for add new place...

---
+++ server/controller/placeController.js
@@ -0,0 +1,9 @@
+export const addNewPlace = async (req, res, next) => {
+  try {
+  } catch (err) {
+    return res.status(500).json({
+      message: "Internal Server Error!",
+      error: err.message,
+    });
+  }
+};
--- server/routes/placeRoutes.js
+++ server/routes/placeRoutes.js
@@ -1,1 +1,10 @@
 import express from "express";
+
+//Router Obj
+const router = express.Router();
+
+//Add New Place
+router.post("/add-new-place");
+
+//Export
+export default router;
