4abb5bbd5db48db751dae20ce8805d1c9c9126d4
Haris Mohanty <harismohanty8658@gmail.com>
2023-12-30 12:26:40 +0530

    server/routes/placeRoutes.js- Routes created for add new place.

--- server/routes/placeRoutes.js
+++ server/routes/placeRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
+import { addNewPlace } from "../controller/placeController.js";
 
 //Router Obj
 const router = express.Router();
 
 //Add New Place
-router.post("/add-new-place");
+router.post("/add-new-place", addNewPlace);
 
 //Export
 export default router;
