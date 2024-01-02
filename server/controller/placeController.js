5e164dee71c304bb3c7d300e137d7f02a2ef76f4
Haris Mohanty <harismohanty8658@gmail.com>
2024-01-02 11:44:14 +0530

    server/controller/placeController.js- Write code for fetch all added places controller funcation code processing....

--- server/controller/placeController.js
+++ server/controller/placeController.js
@@ -77,3 +77,14 @@ export const addNewPlace = async (req, res, next) => {
     });
   }
 };
+
+//****************** GET ALL ADDED PLACES ********************** /
+export const addedPlaces = async (req, res, next) => {
+  try {
+  } catch (err) {
+    return res.status(500).json({
+      message: "Internal Server Error!",
+      error: err.message,
+    });
+  }
+};
