54c854c7e0e71e79892afeb9b1e76e37d64465b3
Haris Mohanty <harismohanty8658@gmail.com>
2024-01-02 11:42:22 +0530

    server/routes/placeRoutes.js- Routes created for get all added places.

--- client/src/components/AddedPlaces.jsx
+++ client/src/components/AddedPlaces.jsx
@@ -1,12 +1,14 @@
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
 const AddedPlaces = () => {
-    useEffect(()=>{},[])
+  useEffect(() => {}, []);
   return (
     <>
-      
+      <div className="mt-4">
+        <div className="bg-gray-200 p-4 rounded-2xl"></div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AddedPlaces
+export default AddedPlaces;
--- server/routes/placeRoutes.js
+++ server/routes/placeRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { addNewPlace } from "../controller/placeController.js";
+import { addNewPlace, addedPlaces } from "../controller/placeController.js";
 
 //Router Obj
 const router = express.Router();
 
 //Add New Place
 router.post("/add-new-place", addNewPlace);
+
+//Get all addeded places
+router.get("/added-places", addedPlaces);
 
 //Export
 export default router;
