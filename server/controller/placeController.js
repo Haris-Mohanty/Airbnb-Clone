2d0b531ea353ac56449bfdefc261a360f41e79cd
Haris Mohanty <harismohanty8658@gmail.com>
2024-01-02 11:50:25 +0530

    server/controller/placeController.js- Write code for fetch all added places controller funcation code fixed.

--- server/controller/placeController.js
+++ server/controller/placeController.js
@@ -81,6 +81,36 @@ export const addNewPlace = async (req, res, next) => {
 //****************** GET ALL ADDED PLACES ********************** /
 export const addedPlaces = async (req, res, next) => {
   try {
+    //Get Token || Token validation
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized: Token not provided.",
+      });
+    }
+
+    //Verify Token and Get User ID
+    let userId;
+    try {
+      const decode = await jwt.verify(token, process.env.JWT_SECRET);
+      userId = decode.userId;
+    } catch (err) {
+      return res.status(401).json({
+        message: "Invalid Token!",
+      });
+    }
+
+    const addedPlace = await PlaceModel.find({ owner: userId });
+
+    if (addedPlaces.length === 0) {
+      return res.status(404).json({
+        message: "No places found for the user.",
+      });
+    }
+
+    return res.status(200).json({
+      addedPlace,
+    });
   } catch (err) {
     return res.status(500).json({
       message: "Internal Server Error!",
