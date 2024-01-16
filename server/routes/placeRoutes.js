37eadb2df4b0422000f8413edb93043448ffa77d
Haris Mohanty <harismohanty8658@gmail.com>
2024-01-16 11:24:50 +0530

    server/routes/placeRoutes.js- Create routes for get all added places.

--- server/routes/placeRoutes.js
+++ server/routes/placeRoutes.js
@@ -4,6 +4,7 @@ import {
   addedPlaces,
   getPlacesById,
   updatePlace,
+  getAllAddedPlaces,
 } from "../controller/placeController.js";
 
 //Router Obj
@@ -12,14 +13,17 @@ const router = express.Router();
 //Add New Place
 router.post("/add-new-place", addNewPlace);
 
-//Get all addeded places
+//Get all addeded places (By User)
 router.get("/added-places", addedPlaces);
 
-//Get places by ID
+//Get places by ID (By Place ID)
 router.get("/places/:id", getPlacesById);
 
 //Update Place
 router.put("/updatePlace", updatePlace);
+
+//Get all added Places (Home page)
+router.get("/all-added-places", getAllAddedPlaces);
 
 //Export
 export default router;
