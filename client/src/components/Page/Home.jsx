d58162161365c873b9bb896a22de6cd8b92d4c4e
Haris Mohanty <harismohanty8658@gmail.com>
2024-01-18 22:32:20 +0530

    client/src/components/Page/Home.jsx- when click on a place show full details of place....

--- client/src/components/Page/Home.jsx
+++ client/src/components/Page/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getAllAddedPlaces } from "../../api/api";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const [places, setPlaces] = useState([]);
@@ -22,7 +23,7 @@ const Home = () => {
       <div className="mt-8 gap-x-6 gap-y-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
         {places.length > 0 &&
           places.map((place, index) => (
-            <div key={index}>
+            <Link to={"/place/" + place._id} key={index}>
               <div className="bg-gray-500 mb-2 rounded-2xl flex">
                 {place.photos?.[0] && (
                   <img
@@ -34,8 +35,10 @@ const Home = () => {
               </div>
               <h2 className="font-bold">{place.address}</h2>
               <h3 className="text-sm text-gray-500">{place.title}</h3>
-              <div className="mt-2">${place.price} per night</div>
-            </div>
+              <div className="mt-1">
+                <span className="font-bold">${place.price} per night</span>
+              </div>
+            </Link>
           ))}
       </div>
     </>
