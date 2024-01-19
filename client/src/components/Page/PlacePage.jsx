4754e03de161ef84e208b5ea65b89dcfb25380dd
Haris Mohanty <harismohanty8658@gmail.com>
2024-01-19 23:27:50 +0530

    client/src/components/Page/PlacePage.jsx- Get place by id code processing....

--- client/src/components/Page/PlacePage.jsx
+++ client/src/components/Page/PlacePage.jsx
@@ -1,8 +1,24 @@
+import { useParams } from "react-router-dom";
+import { getPlacesById } from "../../api/api";
+
 const PlacePage = () => {
+  const { id } = useParams();
+
+  const fetchPlaceById = async () => {
+    try {
+      const data = getPlacesById(id);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div>
-      <h2>Hello</h2>
-    </div>
+    <>
+      <div className="mt-8">
+        <h1></h1>
+      </div>
+    </>
   );
 };
 
