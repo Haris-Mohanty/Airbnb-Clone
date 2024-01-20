2243c06ed94f7dacc650b7d6672ff5a35e499d07
Haris Mohanty <harismohanty8658@gmail.com>
2024-01-20 11:44:47 +0530

    client/src/components/Page/PlacePage.jsx- Show place details code processing........

--- client/src/components/Page/PlacePage.jsx
+++ client/src/components/Page/PlacePage.jsx
@@ -1,28 +1,35 @@
 import { useParams } from "react-router-dom";
 import { getPlacesById } from "../../api/api";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const PlacePage = () => {
   const { id } = useParams();
+  const [place, setPlace] = useState(null);
 
   //Fetch places from ID
   const fetchPlaceById = async (id) => {
     try {
       const data = await getPlacesById(id);
-      console.log(data);
+      setPlace(data.place);
     } catch (err) {
       console.log(err);
     }
   };
 
   useEffect(() => {
+    if (!id) {
+      return;
+    }
+
     fetchPlaceById(id);
-  }, []);
+  }, [id]);
+
+  if (!place) return "";
 
   return (
     <>
       <div className="mt-8">
-        <h1></h1>
+        <h1>{place.title}</h1>
       </div>
     </>
   );
