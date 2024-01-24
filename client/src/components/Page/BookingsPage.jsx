e3cde32e6cc43993aef976c41b11939ff122d3d1
Haris Mohanty <harismohanty8658@gmail.com>
2024-01-24 10:15:49 +0530

    client/src/components/Page/BookingPage.jsx- All booking of user fetched successfully.

--- client/src/components/Page/BookingsPage.jsx
+++ client/src/components/Page/BookingsPage.jsx
@@ -1,6 +1,21 @@
+import { useEffect } from "react";
+import { getAllBookingOfUser } from "../../api/api";
 import AccountNav from "../AccountNav";
 
 const BookingsPage = () => {
+  //Fetch all booking of user
+  const fetchBookings = async () => {
+    try {
+      const { allBookingsOfUser } = await getAllBookingOfUser();
+      console.log(allBookingsOfUser);
+    } catch (err) {
+      alert(err.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchBookings();
+  }, []);
   return (
     <>
       <AccountNav />
