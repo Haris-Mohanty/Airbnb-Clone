9e090dfabb1cca5d5319af094fe3c657a3dde506
Haris Mohanty <harismohanty8658@gmail.com>
2024-01-27 21:23:42 +0530

    server/routes/bookingRoutes.js- Get Booking Details routes created.

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   bookingPlace,
   getBookingOfUser,
+  getBookingDetails,
 } from "../controller/bookingController.js";
 
 //Router Obj
@@ -12,6 +13,9 @@ router.post("/bookings", bookingPlace);
 
 // Get all booking of user (By User Id)
 router.get("/getBookings", getBookingOfUser);
+
+//Get booking details (By Booking Id)
+router.get("/getBooking/:id", getBookingDetails);
 
 //Export
 export default router;
