385d55ad3389462f00241830d408b5e88098a050
Haris Mohanty <harismohanty8658@gmail.com>
2024-01-27 10:13:54 +0530

    client/src/components/Page/BookingPage.jsx- All booking show on page code fixed.

--- client/src/components/Page/BookingsPage.jsx
+++ client/src/components/Page/BookingsPage.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { getAllBookingOfUser } from "../../api/api";
 import AccountNav from "../AccountNav";
-import { format } from "date-fns";
+import { differenceInCalendarDays, format } from "date-fns";
+import { Link } from "react-router-dom";
 
 const BookingsPage = () => {
   const [bookings, setBookings] = useState([]);
@@ -24,7 +25,8 @@ const BookingsPage = () => {
       <div>
         {bookings?.length > 0 &&
           bookings.map((booking, index) => (
-            <div
+            <Link
+              to={`/account/bookings/${booking._id}`}
               key={index}
               className="flex gap-4 bg-gray-200 rounded-xl overflow-hidden mb-2"
             >
@@ -39,12 +41,49 @@ const BookingsPage = () => {
                   />
                 )}
               </div>
-              <div className="py-3">
+              <div className="py-3 pr-3 grow">
                 <h2 className="text-xl">{booking.place.title}</h2>
-                {format(new Date(booking.checkIn), "yyyy-MM-dd")} {"->"}{" "}
-                {format(new Date(booking.checkOut), "yyyy-MM-dd")}
+                <div className="border-t flex border-gray-300 mt-2 py-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+                    />
+                  </svg>
+                  {format(new Date(booking.checkIn), "yyyy-MM-dd")} &rarr;{"  "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+                    />
+                  </svg>
+                  {format(new Date(booking.checkOut), "yyyy-MM-dd")}
+                </div>
+                <div className="font-medium">
+                  {differenceInCalendarDays(
+                    new Date(booking.checkOut),
+                    new Date(booking.checkIn)
+                  )}{" "}
+                  Nights | Total Price: ₹{booking.price}
+                </div>
               </div>
-            </div>
+            </Link>
           ))}
       </div>
     </>
