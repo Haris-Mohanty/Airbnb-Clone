0d72582beafbc49abc20cd136053fb8bcc002432
Haris Mohanty <harismohanty8658@gmail.com>
2024-01-29 09:39:31 +0530

    client/src/components/Page/BookingPlacePage.jsx- Fetch booking details successfully.

--- client/src/components/Page/BookingPlacePage.jsx
+++ client/src/components/Page/BookingPlacePage.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getBookingDetails } from "../../api/api";
 
 const BookingPlacePage = () => {
   const { id } = useParams();
+  const [booking, setBookings] = useState(null);
+
+  //Fetch Booking Details
+  const fetchBookingDetails = async (id) => {
+    try {
+      getBookingDetails(id);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (id) {
+      fetchBookingDetails(id);
+    }
+  }, [id]);
   return (
     <>
       <div>{id}</div>
